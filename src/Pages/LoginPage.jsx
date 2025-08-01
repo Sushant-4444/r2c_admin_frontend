e75751c7daf20095d03b160f7aed665758f9951a
Sushant-4444 <sushant23537@iiitd.ac.in>
2025-08-01 22:11:03 +0530

    update

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -72,7 +72,7 @@ const LoginPage = () => {
       console.log("Google sign-in successful. Token:", token);
 
       // Call backend to check admin status
-      const response = await fetch("http://localhost:8080/auth/google-signin", {
+      const response = await fetch("https://r2c.iiitd.edu.in/admin/api/auth/google-signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/Pages/Research.jsx
+++ src/Pages/Research.jsx
@@ -593,7 +593,7 @@ const Research = () => {
 
   const [sortConfig, setSortConfig] = useState({ key: 'createdDate', direction: 'descending' });
 
-  const API_BASE_URL = "http://localhost:8080";
+  const API_BASE_URL = "https://r2c.iiitd.edu.in/admin/api";
 
   const getIdToken = async () => {
     const user = auth.currentUser;
