38860aee81160538a399406037a4e53d448a8a97
Sushant-4444 <sushant23537@iiitd.ac.in>
2025-08-06 14:58:30 +0530

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -41,7 +41,9 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
+const db = getFirestore(app);
+
 const googleProvider = new GoogleAuthProvider();
 
 
-export { auth, googleProvider };
+export { auth, googleProvider, db };
